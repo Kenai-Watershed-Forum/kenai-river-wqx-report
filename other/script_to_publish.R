eaf354c9705b3645a5d76abf22d06c5eba5acee8
benjamin-meyer-ak <69653328+benjamin-meyer-ak@users.noreply.github.com>
2022-02-16 15:10:33 -0900

    commit

--- other/script_to_publish.R
+++ other/script_to_publish.R
@@ -4,6 +4,9 @@
 # clean out old versions
 bookdown::clean_book(TRUE)
 
+# render new version
+bookdown:: render_book()
+
 # publish book to https://bookdown.org/kwfwqx/Kenai_River_Water_Quality_Assessment/
 bookdown::publish_book(name = "Kenai_River_Water_Quality_Assessment", account = "kwfwqx")
 
