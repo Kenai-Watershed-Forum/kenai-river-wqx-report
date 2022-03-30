103e653bbbaf243afe1cb6367cd36873a5b061db
benjamin-meyer-ak <69653328+benjamin-meyer-ak@users.noreply.github.com>
2022-03-30 11:44:32 -0800

    commit

--- other/script_to_publish.R
+++ other/script_to_publish.R
@@ -1,5 +1,5 @@
 
-# SYNCH/PUBLISH BOOK TO BOOKDOWN.ORG
+# SYNCH/PUBLISH BOOK TO BOOKDOWN.ORG (created this becuase the 'publish' button in corner of html interface window after render does not work)
 
 # clean out old versions
 bookdown::clean_book(TRUE)
@@ -9,4 +9,4 @@ bookdown:: render_book()
 
 # publish book to https://bookdown.org/kwfwqx/Kenai_River_Water_Quality_Assessment/
 bookdown::publish_book(name = "Kenai_River_Water_Quality_Assessment", account = "kwfwqx")
-
+Y
