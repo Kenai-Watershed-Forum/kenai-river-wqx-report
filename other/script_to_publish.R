2b278677ef7b83edeaf33f34f1c36c562ed2a875
benjamin-meyer-ak <69653328+benjamin-meyer-ak@users.noreply.github.com>
2022-12-15 18:03:38 -0900

    commit

--- other/script_to_publish.R
+++ other/script_to_publish.R
@@ -10,3 +10,21 @@ bookdown:: render_book()
 # publish book to https://bookdown.org/kwfwqx/Kenai_River_Water_Quality_Assessment/
 bookdown::publish_book(name = "Kenai_River_Water_Quality_Assessment", account = "kwfwqx")
 
+# note 12/15/2022 -- the command blogdown:::serve_site() appears to no longer function for most
+# bookdown renders; including this one.
+
+
+# run this function manually to create auto-refreshing preview in web browser.
+# https://bookdown.org/yihui/bookdown/serve-the-book.html
+
+# load packages
+library(bookdown)
+library(servr)
+
+# serve book
+serve_book(dir = ".", output_dir = "_book", preview = TRUE,
+           in_session = TRUE, quiet = FALSE)
+
+# as of 12/15/2022, "Error: pandoc document conversion failed with error 64"
+# maybe more efficient to just convert to Quarto to proceed????
+
